26b0d9470a4d3c96ec5a82ed9fabdf135343a99e
wycers <wycers@gmail.com>
2022-11-14 21:48:02 +0800

    ✨ change ws to socket.io

--- src/ws/ws.gateway.ts
+++ src/ws/ws.gateway.ts
@@ -1,53 +1,66 @@
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Socket, Server } from "ws";
-import * as pty from "node-pty"
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import * as pty from 'node-pty';
 
-
-@WebSocketGateway(4000)
-export class WsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+@WebSocketGateway({
+  cors: {
+    origin: 'http://127.0.0.1:5173',
+  },
+})
+export class WsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server: Server;
 
-  afterInit(server: Server) {
-    console.log(server);
-    //Do stuffs
-  }
+  afterInit(server: Server) {}
 
   handleDisconnect(client: Socket) {
     console.log(`Disconnected: ${client.id}`);
     //Do stuffs
+    client.removeAllListeners();
   }
 
   handleConnection(client: Socket, ...args: any[]) {
-    console.log(client)
+    // console.log(client);
     console.log(`Connected ${client.id}`);
-    //Do stuffs
 
-    const term = pty.spawn("bash", ["--login"], {
-      name: "xterm-color",
-      cols: 80,
-      rows: 24,
-      cwd: process.env.HOME,
-      env: process.env,
-    });
+    // term.onData(function (data) {
+    //   client.send(data);
+    // });
 
-    term.onData(function (data) {
-      console.log(data)
-      client.send(data);
-    });
-
-    client.on("message", (data) => {
-      console.log(data)
-      term.write(data);
-    });
+    // client.on("message", (data) => {
+    //   term.write(data);
+    // });
   }
 
+  @SubscribeMessage('register')
+  handleRegister(client: Socket, data) {
+    console.log(data);
+    if (data === 'USER') {
+      // const term = pty.spawn("ssh", ["-p", "2222", "-t", "root@127.0.0.1", 'podman run --rm -it ubuntu /bin/bash'], {
+      // const term = pty.spawn("podman", ["run", "--rm", "-it", "-v", "/home/hitomi/Developer/seed/Labsetup/server-code:/bof", "ubuntu", "/bin/bash"], {
+      const term = pty.spawn('bash', ['--login'], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 24,
+        cwd: process.env.HOME,
+        env: process.env,
+      });
+      client.on('term-input', (data) => term.write(data));
+      term.onData((data) => client.emit('term-output', data));
+    }
+  }
 
-  @SubscribeMessage('hello')
-  hello(@MessageBody() data: any): any {
-    return {
-      "event": "hello",
-      "data": data,
-      "msg": 'rustfisher.com'
-    };
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, data: string) {
+    console.log(data);
+    client.emit('env', 'CREATE');
   }
 }
